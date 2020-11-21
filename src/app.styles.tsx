6e04229858b29c47c554f29df2c4d3f72265d49c
DenisEps <denis.epshteyn@gmail.com>
2020-11-21 14:07:18 +0300

    adds react transition group to animate switching between pages

--- src/app.styles.tsx
+++ src/app.styles.tsx
@@ -5,4 +5,21 @@ const { Content } = Layout;
 
 export const ContentContainer = styled(Content)`
   padding: 1rem;
+
+  .fade-enter {
+    opacity: 0.01;
+  }
+
+  .fade-enter.fade-enter-active {
+    opacity: 1;
+    transition: opacity 300ms ease-in;
+  }
+
+  .fade-exit {
+    opacity: 0;
+  }
+
+  .fade-exit.fade-exit-active {
+    opacity: 0;
+  }
 `;
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { Layout } from "antd";
 import "app.css";
 
@@ -7,22 +8,30 @@ import { ContentContainer } from "app.styles";
 
 import { HeaderComponent } from "components/header/Header";
 import { HomePage } from "pages/homepage/HomePage";
-import {LibraryDetails} from 'pages/library-details/LibraryDetails';
+import { LibraryDetails } from "pages/library-details/LibraryDetails";
 
 const App: React.FC = () => {
-
+  const location = useLocation();
   return (
-      <Layout>
+      <Layout style={{ height: "100vh" }}>
         <HeaderComponent />
         <ContentContainer>
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route path="/libraries/:id">
-              <LibraryDetails />
-            </Route>
-          </Switch>
+          <TransitionGroup>
+            <CSSTransition
+              key={location.key}
+              timeout={{ enter: 300, exit: 300 }}
+              classNames={"fade"}
+            >
+              <Switch location={location}>
+                <Route exact path="/">
+                  <HomePage />
+                </Route>
+                <Route path="/libraries/:id">
+                  <LibraryDetails />
+                </Route>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
         </ContentContainer>
         {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
       </Layout>
